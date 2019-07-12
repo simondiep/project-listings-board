5a6e857107378ec1160bff470f0b5b96171e1678
Simon Diep <simon.diep@rallyhealth.com>
2019-07-12 15:54:40 -0500

    Updates

---
+++ precache-manifest.923f268bfb303c2a5b8d29d3d2a06270.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8e9aa25fbd4f84918711efde3067f6",
+    "url": "/project-listings-board/index.html"
+  },
+  {
+    "revision": "91295a0ba19b80fbda55",
+    "url": "/project-listings-board/static/css/main.665d8dda.chunk.css"
+  },
+  {
+    "revision": "c8c9462e1fde78bdc890",
+    "url": "/project-listings-board/static/js/2.f18b225f.chunk.js"
+  },
+  {
+    "revision": "91295a0ba19b80fbda55",
+    "url": "/project-listings-board/static/js/main.14749eb7.chunk.js"
+  },
+  {
+    "revision": "5523dd30fee0cc8bb85e",
+    "url": "/project-listings-board/static/js/runtime~main.6ec5ea55.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/project-listings-board/precache-manifest.923f268bfb303c2a5b8d29d3d2a06270.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-listings-board/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
